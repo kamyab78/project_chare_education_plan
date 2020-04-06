45259b7903b77d672d1e998977897c2f131ba9b4
kamyab <amirhosseinghassemi78@yahoo.com>
2020-04-06 23:11:36 +0430

    token validation

--- app/src/main/java/com/example/app_/MainActivity.java
+++ app/src/main/java/com/example/app_/MainActivity.java
@@ -13,9 +13,13 @@ import android.widget.Toast;
 
 import com.example.app_.activity.first_page;
 import com.example.app_.activity.login;
+import com.example.app_.activity.patern;
+import com.example.app_.activity.plan;
 import com.example.app_.activity.sabt_nam;
 import com.example.app_.base.request_base;
 import com.example.app_.entity.information;
+import com.example.app_.entity.plan_;
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -61,8 +65,60 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
         else {
-            Intent intent=new Intent(MainActivity.this, first_page.class);
-            startActivity(intent);
+            try {
+                FileInputStream fileInputStream = openFileInput("data.txt");
+                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line;
+                stringBuffer = new StringBuffer();
+                while ((line = bufferedReader.readLine()) != null)
+                    stringBuffer.append(line);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String token = stringBuffer.toString();
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("application/json");
+//            RequestBody body = RequestBody.create(mediaType, "{\n" +
+//                    "  \"pattern\": " + name_id.get(patterns) + ",\n" +
+//                    "  \"for_exam\": " + 3 + "\n" +
+//                    "}");
+            Request request = new Request.Builder()
+                    .url("http://194.5.207.137:8000/api/v1/users/main/profile/")
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("Authorization", "token " + token)
+//                    .method("POST", body)
+                    .build();
+
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    System.out.println("fail");
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String body=response.body().string();
+//                    System.out.println(body);
+                    System.out.println(response.code());
+                    if(response.code()==200){
+                        Intent intent=new Intent(MainActivity.this, first_page.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Intent intent=new Intent(MainActivity.this, login.class);
+                        startActivity(intent);
+                    }
+
+                }
+            });
+
         }
 
     }
